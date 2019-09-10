a3908013f51766d999342dabe37320932cda413e
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2019-09-10 16:37:10 -0300

    Initial Commit

---
+++ src/config/auth.js
@@ -0,0 +1,5 @@
+export default {
+  // gobarbercezarmzz
+  secret: 'e3e3c758e0755a0b65187cb9834b4ba5',
+  expiresIn: '7d',
+};
---
+++ src/database/index.js
@@ -0,0 +1,26 @@
+import Sequelize from 'sequelize';
+
+import User from '../app/models/User';
+import File from '../app/models/File';
+
+import databaseConfig from '../config/database';
+
+const models = [User, File];
+
+class Database {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    // cria o objeto de conexão com o banco
+    this.connection = new Sequelize(databaseConfig);
+
+    // inicializa a conexão em cada model
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
+  }
+}
+
+export default new Database();
