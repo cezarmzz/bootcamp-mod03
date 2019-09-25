f401122a13a28e23981975f057856ad20c6d98c5
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2019-09-24 22:24:48 -0300

    Notifications, Email on Appointments canceled and other features.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,16 +1,25 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
 
+import Appointment from '../models/Appointment';
+import Notification from '../schemas/Notification';
+
+import Mail from '../../lib/Mail';
+
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit: 20,
+      offset: (page - 1) * 20,
       include: [
         {
           model: User,
@@ -41,7 +50,15 @@ class AppointmentController {
     }
 
     const { provider_id, date } = req.body;
+    /**
+     * Validate if user isn't a provider
+     */
 
+    if (req.userId === provider_id) {
+      return res
+        .status(401)
+        .json({ error: 'A provider cannot create an appoitment to it self' });
+    }
     /**
      * Check if provider_id is a provider
      */
@@ -82,6 +99,63 @@ class AppointmentController {
       date,
     });
 
+    /**
+     * Notify appointment provider
+     */
+
+    const user = await User.findByPk(req.userId);
+    const dateFormatted = format(hourStart, "'dia' dd 'de' MMMM', às' H'h'mm", {
+      locale: ptBR,
+    });
+    await Notification.create({
+      content: `Novo agendamento de ${user.name} para o ${dateFormatted}`,
+      user: provider_id,
+    });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
+
+    if (!appointment) {
+      return res.status(404).json({ error: 'Appointment not found!' });
+    }
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permissions to cancel this appointment.",
+      });
+    }
+
+    /**
+     * Sub 2 hours from appointment hour
+     * and check if actual date is before date sub
+     */
+    const dateLess = subHours(appointment.date, 2);
+    if (isBefore(dateLess, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel appointments 2 hours in advance.',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
+
+    await Mail.sendMail({
+      to: `${appointment.provider.name} <${appointment.provider.email}>`,
+      subject: 'Agendamento cancelado',
+      text: 'Você tem um novo cancelamento',
+    });
     return res.json(appointment);
   }
 }
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '8d915e286fed98',
+    pass: 'f4186af5014712',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
