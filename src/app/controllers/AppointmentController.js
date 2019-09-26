9d92507931c386c240433387337560528e9901f6
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2019-09-26 14:39:02 -0300

    List available times to appoint, virtual fields past and cancelable, redis config to send emails when appointment is canceled.

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -8,7 +8,8 @@ import File from '../models/File';
 import Appointment from '../models/Appointment';
 import Notification from '../schemas/Notification';
 
-import Mail from '../../lib/Mail';
+import CancellationMail from '../jobs/CancellationMail';
+import Queue from '../../lib/Queue';
 
 class AppointmentController {
   async index(req, res) {
@@ -17,7 +18,7 @@ class AppointmentController {
     const appointments = await Appointment.findAll({
       where: { user_id: req.userId, canceled_at: null },
       order: ['date'],
-      attributes: ['id', 'date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
       limit: 20,
       offset: (page - 1) * 20,
       include: [
@@ -73,7 +74,7 @@ class AppointmentController {
         .json({ error: 'Appointments only can be created with a provider.' });
     }
     /**
-     * Check for past dates
+     * Check for Mailpast dates
      */
 
     const hourStart = startOfHour(parseISO(date));
@@ -123,6 +124,11 @@ class AppointmentController {
           as: 'provider',
           attributes: ['name', 'email'],
         },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['name'],
+        },
       ],
     });
 
@@ -151,11 +157,8 @@ class AppointmentController {
 
     await appointment.save();
 
-    await Mail.sendMail({
-      to: `${appointment.provider.name} <${appointment.provider.email}>`,
-      subject: 'Agendamento cancelado',
-      text: 'Você tem um novo cancelamento',
-    });
+    await Queue.add(CancellationMail.key, { appointment });
+
     return res.json(appointment);
   }
 }
